8c22a1ed736fa191907ee8252bc90916ba8d6c92
David Messerschmitt <messer@DGM-MacBookPro.local>
2018-02-18 23:49:24 -0800

    Created ClientStub() class

--- metadata_demo/metadata_client.py
+++ metadata_demo/metadata_client.py
@@ -11,8 +11,8 @@ import metadata_demo_pb2_grpc as grpcServe
 # Configuration
 _net_connection = 'localhost:50055'
 
-# metadata is represented by a dictionary on both send and receive
-# initialize metadata dictionary
+# messages are represented by a dictionary on both send and receive
+# initialize that dictionary
 _serviceName = 'ServiceControl'
 _rcpNames = ['Query','Service','WrapUp']
 _messageFields =  {
@@ -23,45 +23,51 @@ _messageFields =  {
         'ServiceStatus':    {'report' : None},
         'WrapUpReport':     {'report' : None}
         }
-
 # note to future: would be nice to generate
 #   _meta skeleton automatically from .proto file
 
-def _message(message_name):
-    # message_name = string identifying type of message
-    cmd='grpcMessage.{0}(**_messageFields["{0}"])'.format(message_name)
-    print(cmd)
-    return eval(cmd)
+class ClientStub():
+    '''
+    Establish channel and satisfy rpc requests.
+    Class not specific to any protocol buffer definition.
+    Change .proto file and this class does not require modification.
+    '''
 
-def _grpcRequest(rpc_name,message_name):
-    # rpc_name = string with name of service
-    # message_name = string identifying type of message
-    cmd = 'grpcMessage.{0}(**_messageFields["{0}"])'.format(message_name)
-    cmd = '_stub.{0}({1})'.format(rpc_name,cmd)
-    print(cmd)
-    return eval(cmd)
+    def __init__(self):
+        self.channel = grpc.insecure_channel(_net_connection)
+        cmd = 'grpcServe.{0}Stub(self.channel)'.format(_serviceName)
+        self.stub = eval(cmd)
+        
+    def rpcRequest(self,rpc_name,message_name):
+        # request to invoke rpc requires name of rcp channel and message name
+        # rpc_name = string with name of service
+        # message_name = string identifying type of message
+        cmd = 'grpcMessage.{0}(**_messageFields["{0}"])'.format(message_name)
+        # Example:
+        # cmd = 'grpcMessage.QueryRequest(**_messageFields["QueryRequest"])'
+        cmd = 'self.stub.{0}({1})'.format(rpc_name,cmd)
+        # Example:
+        # cmd = 'self.stub.Query(cmd)'
+        return eval(cmd)
 
   
 def run():
-    _channel = grpc.insecure_channel(_net_connection)
-    cmd = 'grpcServe.{0}Stub(_channel)'.format(_serviceName)
-    _stub = eval(cmd)
+    s = ClientStub()
 
     # example query of the server
     _messageFields['QueryRequest']['question'] = 'What can you do?'
-    _r1 = _stub.Query(_message('QueryRequest'))
-    #_r1 = _grpcRequest('Query','QueryRequest')
-    _messageFields['QueryReply']['answer'] = _r1.answer
+    _r = s.rpcRequest('Query','QueryRequest')
+    _messageFields['QueryReply']['answer'] = _r.answer
 
     # example service request
     _messageFields['ServiceRequest']['request'] = 'Do B please'
-    _r2 = _stub.Service(_message('ServiceRequest'))
-    _messageFields['ServiceStatus']['report'] = _r2.report
+    _r = s.rpcRequest('Service','ServiceRequest')
+    _messageFields['ServiceStatus']['report'] = _r.report
 
     # example wrapup request
     _messageFields['ClientStatus']['report'] = 'B completed as I was expecting'
-    _r3 = _stub.WrapUp(_message('ClientStatus'))
-    _messageFields['WrapUpReport']['report'] = _r3.report
+    _r = s.rpcRequest('WrapUp','ClientStatus')
+    _messageFields['WrapUpReport']['report'] = _r.report
 
     # print messages back from server
     print(_messageFields['QueryReply'])

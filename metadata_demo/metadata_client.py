b96c5d543b96787b9ada8a9dddadd48fefb0ed95
David Messerschmitt <messer@DGM-MacBookPro.local>
2018-02-20 12:44:57 -0800

    Updated server

--- metadata_demo/metadata_client.py
+++ metadata_demo/metadata_client.py
@@ -1,83 +1,120 @@
 """
-The Python implementation of the GRPC helloworld.Greeter client.
-This version modified by David G Messerschmitt
-27 Jan 2018
+The Python implementation of a generic gRPC client stub
+Programmer David G Messerschmitt
+19 Feb 2018
 """
 
 import grpc
 import metadata_demo_pb2 as grpcMessage
 import metadata_demo_pb2_grpc as grpcServe
 
-# Configuration
-_net_connection = 'localhost:50055'
-
-# messages are represented by a dictionary on both send and receive
-# initialize that dictionary
-_serviceName = 'ServiceControl'
-_rcpNames = ['Query','Service','WrapUp']
-_messageFields =  {
-        'QueryRequest':     {'question' : None},
-        'ServiceRequest' :  {'request' : None},
-        'ClientStatus' :    {'report' : None},
-        'QueryReply':       {'answer' : None},
-        'ServiceStatus':    {'report' : None},
-        'WrapUpReport':     {'report' : None}
-        }
-# note to future: would be nice to generate
-#   _meta skeleton automatically from .proto file
-
 class GenericClientStub():
     '''
     Establish channel and satisfy rpc requests
     Class not specific to any protocol buffer definition, but encapsulates
-        all the grpc mechanics
-    Change .proto file and this class does not require modification
+        all the grpc mechanics and hides them from the user
+    This class does not require modification if the .proto file is changed
     '''
 
     def __init__(self):
-        self.channel = grpc.insecure_channel(_net_connection)
-        cmd = 'grpcServe.{0}Stub(self.channel)'.format(_serviceName)
+        self.channel = grpc.insecure_channel(_NET_CONNECTION)
+        cmd = 'grpcServe.{0}Stub(self.channel)'.format(self.serviceName)
         self.stub = eval(cmd)
         
-    def rpcRequest(self,rpc_name,message_name):
-        # request to invoke rpc requires name of rcp channel and message name
+    def rpcRequest(self,rpc_name):
+        # request to invoke rpc requires name of rcp channel
         # rpc_name = string with name of service
-        # message_name = string identifying type of message
-        cmd = 'grpcMessage.{0}(**_messageFields["{0}"])'.format(message_name)
+
+        # look up name of client message
+        client_message_name = self.rcpAndMessageNames[rpc_name][0]
+        # Example:
+        # self.client_message_name = 'QueryRequest'
+
+        # formulate gRPC command in two stages
+        cmd = 'grpcMessage.{0}(**self.messageFields["{0}"])'.format(client_message_name)
         # Example:
         # cmd = 'grpcMessage.QueryRequest(**_messageFields["QueryRequest"])'
+        
         cmd = 'self.stub.{0}({1})'.format(rpc_name,cmd)
         # Example:
         # cmd = 'self.stub.Query(cmd)'
-        return eval(cmd)
+
+        # execute message transmission
+        r = eval(cmd)
+    
+        # look up name of server response message
+        server_message_name = self.rcpAndMessageNames[rpc_name][1]
+        # Example:
+        # server_message_name = 'QueryReply'
+        
+        # populate the dictionary message fields based on response r
+        server_message_keys = self.messageFields[server_message_name]
+        for key in server_message_keys:
+            self.messageFields[server_message_name][key] = eval('r.{0}'.format(key))
+
+    def report(self):
+        print('\n!!!!! Report of the gRPC sent and response messages !!!!')
+        for rpc_name in self.rcpAndMessageNames.keys():
+             print('\nRPC name: ' + rpc_name)
+             for message_name in self.rcpAndMessageNames[rpc_name]:
+                 print('\tMessage name: ' + message_name)
+                 for field_name in self.messageFields[message_name].keys():
+                     field_value = self.messageFields[message_name][field_name]
+                     print('\t\t' + field_name + ' = ' + field_value)
+
+# Configuration
+_NET_CONNECTION = 'localhost:50055'
 
 class MetadataClient(GenericClientStub):
+    '''
+    This is the user-programmed class specific to a particular .proto file
+    All message fields are stored in a dictionary messageFields
+    Interactions with gRPC is thru setting and reading values in this dictionary
+        and calling methods of super() = GenericClientStub()
+    '''
 
     def __init__(self):
+
+        # drawn from .proto file
+        self.serviceName = 'ServiceControl'
+
+        # dictionaries of rpc and message names must align with .proto file
+        self.rcpAndMessageNames = {
+                                    'Query' :      ['QueryRequest',    'QueryReply'    ],
+                                    'Service' :    ['ServiceRequest',  'ServiceStatus' ],
+                                    'WrapUp':      ['ClientStatus',    'WrapUpReport'  ]
+                                  }
+
+        # any desired default values can be introduced here in place of None
+        # any message fields not defaulted or set here will default to gRPC-defined values
+        self.messageFields =    {
+                                'QueryRequest':     {'question' : None  },
+                                'ServiceRequest' :  {'request'  : None  },
+                                'ClientStatus' :    {'report'   : None  },
+                                'QueryReply':       {'answer'   : None  },
+                                'ServiceStatus':    {'report'   : None  },
+                                'WrapUpReport':     {'report'   : None  }
+                                }
+
         super().__init__()
 
     def run(self):
 
         # example query of the server
-        _messageFields['QueryRequest']['question'] = 'What can you do?'
-        r = self.rpcRequest('Query','QueryRequest')
-        _messageFields['QueryReply']['answer'] = r.answer
+        self.messageFields['QueryRequest']['question'] = 'What services do you offer?'
+        self.rpcRequest('Query')
 
         # example service request
-        _messageFields['ServiceRequest']['request'] = 'Do B please'
-        r = self.rpcRequest('Service','ServiceRequest')
-        _messageFields['ServiceStatus']['report'] = r.report
+        self.messageFields['ServiceRequest']['request'] = 'Provide B please'
+        self.rpcRequest('Service')
 
         # example wrapup request
-        _messageFields['ClientStatus']['report'] = 'B completed as I was expecting'
-        r = self.rpcRequest('WrapUp','ClientStatus')
-        _messageFields['WrapUpReport']['report'] = r.report
+        self.messageFields['ClientStatus']['report'] = 'Service B completed as I was expecting'
+        self.rpcRequest('WrapUp')
 
-        # print messages back from server
-        print(_messageFields['QueryReply'])
-        print(_messageFields['ServiceStatus'])
-        print(_messageFields['WrapUpReport'])
     
 
 if __name__ == '__main__':
-  MetadataClient().run()
+ c = MetadataClient()
+ c.run()
+ c.report()
--- metadata_demo/metadata_server.py
+++ metadata_demo/metadata_server.py
@@ -1,7 +1,7 @@
 """
-The Python implementation of the GRPC helloworld.Greeter client.
-This version modified by David G Messerschmitt
-27 Jan 2018
+The Python implementation of a generic GRPC service stub
+Progammer David G Messerschmitt
+19 Feb 2018
 """
 
 from concurrent import futures
@@ -13,26 +13,51 @@ import metadata_demo_pb2_grpc as grpcServe
 # Configuration
 _MAXIMUM_SERVICE_TIME_IN_MINUTES = 15
 _net_connection = 'localhost:50055'
-
+service_name = 'ServiceControl'
 
 class ServiceControl(grpcServe.ServiceControlServicer):
 
   def __init__(self):
+
+    # drawn from .proto file
+    self.serviceName = 'ServiceControl'
+
+    # dictionaries of rpc and message names must align with .proto file
+    self.rcpAndMessageNames = {
+                                'Query' :      ['QueryRequest',    'QueryReply'    ],
+                                'Service' :    ['ServiceRequest',  'ServiceStatus' ],
+                                'WrapUp':      ['ClientStatus',    'WrapUpReport'  ]
+                              }
+
+    # any desired default values can be introduced here in place of None
+    # any message fields not defaulted or set here will default to gRPC-defined values
+    self.messageFields =    {
+                              'QueryRequest':     {'question' : None  },
+                              'ServiceRequest' :  {'request'  : None  },
+                              'ClientStatus' :    {'report'   : None  },
+                              'QueryReply':       {'answer'   : None  },
+                              'ServiceStatus':    {'report'   : None  },
+                              'WrapUpReport':     {'report'   : None  }
+                            }
+
     super().__init__()
     self.going = True
 
   def Query(self, request, context):
-    print('Server received: ' + request.question)
-    return grpcMessage.QueryReply(answer = 'QueryReply: ' + 'A or B or C')
+    self.messageFields['QueryReply']['answer'] = 'A or B or C'
+    r = self.messageFields['QueryReply']
+    return grpcMessage.QueryReply(**r)
   
   def Service(self, request, context):
-    print('Server received: ' + request.request)        
-    return grpcMessage.ServiceStatus(report = 'ServiceStatus: ' + 'B accomplished')
+    self.messageFields['ServiceStatus']['report'] = 'B accomplished'
+    r = self.messageFields['ServiceStatus']
+    return grpcMessage.ServiceStatus(**r)
 
   def WrapUp(self, request, context):
-    print('Server receive: ' + request.report)
     #self.going = False  # stop server after response is sent
-    return grpcMessage.WrapUpReport(report = 'WrapUpReport: ' + 'Waiting for another request')
+    self.messageFields['WrapUpReport']['report'] = 'Waiting for another request'
+    r = self.messageFields['WrapUpReport']
+    return grpcMessage.WrapUpReport(**r)
 
 def serve():
   _timeout = _MAXIMUM_SERVICE_TIME_IN_MINUTES
